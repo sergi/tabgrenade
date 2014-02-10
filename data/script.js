8ba25e7d614e95bff7326f01afe0b07ff2a6c820
Sergi Mansilla <sergi.mansilla@gmail.com>
2014-02-10 23:45:44 +0100

    Added backend connection and improved UX

--- data/script.js
+++ data/script.js
@@ -1,4 +1,8 @@
 var timeFormat = 'MMMM Do YYYY, h:mm:ss a'
+Parse.initialize("dibRma54UIQ0UYErXdDV0EPdk32AtUSEBQll0Lc7",
+  "iwP0ckwxi7g8ZVWVaJwoel61ckdoUbPPuj2OPPXR");
+
+var TabGroup = Parse.Object.extend("TabGroup");
 
 self.port.on("allTabs", tabs => {
   var tabsByTime = tabs.reduce((prev, curr) => {
@@ -7,16 +11,21 @@ self.port.on("allTabs", tabs => {
     return prev
   }, {});
 
-  for (var key in tabsByTime) {
+  var sortedKeys = Object.keys(tabsByTime).map(x=>parseInt(x)).sort((a, b) => {
+    return a - b;
+  }).reverse();
+
+  sortedKeys.forEach(function(key) {
+    key = key + '';
     var len = tabsByTime[key].length;
     var formattedTime = moment(parseInt(key)).format(timeFormat);
     var template = '<ul>\
       <h1 style="display: inline-block">' + len + ( len > 1 ? ' tabs' : ' tab') + '</h1>\
       <div style="display: inline-block; padding-left: 28px; vertical-align: middle;">\
         <div class="created_on">Created on ' + formattedTime + '</div>\
-        <div class="restore_all">Restore all</div>\
-        <div class="delete_all">Delete all</div>\
-        <div class="open_page">Share as web page</div>\
+        <div class="restore_all">Open all</div>\
+        <div class="delete_all">Remove all</div>\
+        <div class="open_page" data-id="' + key + '">Share as web page</div>\
       </div>\
       {{#' + key + '}}\
       <li><a href="{{{url}}}">{{title}}</a></li>\
@@ -25,14 +34,36 @@ self.port.on("allTabs", tabs => {
 
     document.getElementById("container").innerHTML +=
       Mustache.render(template, tabsByTime);
+  });
+
+  document.addEventListener('click', function(e) {
+    var t = e.target;
+
+    if (t.classList.contains('open_page')) {
+      var group = new TabGroup();
+      var obj = tabsByTime[t.getAttribute('data-id')]
+      obj.time = null;
+      group.set("tabs", obj);
+      group.save(null, {
+        success: function(obj) {
+          console.log('yay!', obj)
+        },
+        error: function(model, error) {
+          console.log(model, error.toSource())
+        }
+      });
+    }
+  });
+
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  function guid() {
+    return s4() + s4() + '' + s4() + '' + s4() + '' +
+      s4() + '' + s4() + s4() + s4();
   }
 });
 
-
-document.addEventListener('click', function(e) {
-  var t = e.target;
-
-  if (t.classList.contains('open_page')) {
-    alert(t.getAttribute('data-id'))
-  }
-});
